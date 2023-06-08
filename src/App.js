777d47589d4784a7943cc99e60674b740aac4b22
dev9sparsh <sparsh.dev9@gmail.com>
2023-06-09 02:53:52 +0530

    Embedding PodCastSearch component in main app component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { ChakraProvider, Box, VStack, Grid, theme } from '@chakra-ui/react';
+import PodCastSearch from './pages/PodCastSearch';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+      <Box>
+        <Grid p={3}>
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <PodCastSearch />
           </VStack>
         </Grid>
       </Box>
