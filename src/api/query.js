42ba97e5b9e8f78256293cd81425ea4aac76abeb
dev9sparsh <sparsh.dev9@gmail.com>
2023-06-09 03:40:01 +0530

    gql query definition scoping filtered list on podcast

---
+++ src/api/query.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export const GET_PODCASTS = gql`
+  query ($limit: Int!, $searchKeyword: String) {
+    contentCards(
+      filter: { types: [PODCAST], limit: $limit, keywords: $searchKeyword }
+    ) {
+      edges {
+        ...PodcastFields
+      }
+      meta {
+        ...MetaDataFields
+      }
+    }
+  }
+
+  fragment PodcastFields on Podcast {
+    name
+    image {
+      uri
+    }
+    categories {
+      name
+    }
+    experts {
+      firstName
+      lastName
+      title
+      company
+    }
+  }
+
+  fragment MetaDataFields on PageInfo {
+    total
+    recommendationId
+    offset
+    limit
+  }
+`;
