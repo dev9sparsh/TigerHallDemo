f5bacfb8f6cedf94f18e71425099798e7a9de9c5
dev9sparsh <sparsh.dev9@gmail.com>
2023-06-09 05:16:51 +0530

    Consuming Api callback , setting up debounce mechanism

--- src/components/SearchToolBar.js
+++ src/components/SearchToolBar.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
 
 import {
   Input,
@@ -8,7 +9,40 @@ import {
 } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 
-const SearchToolBar = () => {
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+const SearchToolBar = ({ ApiHook}) => {
+  let default_limit = 5;
+  const [inputValue, setInputValue] = useState('');
+  const debouncedValue = useDebounce(inputValue, 300);
+
+  // The Debouncing Hook Effect 
+  useEffect(() => {
+    if (debouncedValue) {
+      ApiHook({
+        variables: { limit: default_limit, searchKeyword: debouncedValue },
+      });
+    } else {
+      ApiHook({
+        variables: { limit: default_limit, searchKeyword: '' },
+      });
+    }
+  }, [debouncedValue]);
+
   return (
     <>
       <SimpleGrid w={{ base: '100%', sm: '50%', md: '50%' }} p="4">
@@ -17,7 +51,12 @@ const SearchToolBar = () => {
             pointerEvents="none"
             children={<SearchIcon color="white" />}
           />
-          <Input placeholder="Search" size="md" />
+          <Input
+            placeholder="Search"
+            size="md"
+            value={inputValue}
+            onChange={event => setInputValue(event.target.value)}
+          />
         </InputGroup>
       </SimpleGrid>
     </>
