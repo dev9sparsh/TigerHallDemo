c923af0d59a7e6297dc0e8d15862bda7eadb2faf
dev9sparsh <sparsh.dev9@gmail.com>
2023-06-09 05:12:08 +0530

    Extending chakra provider with custom theming

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import React from 'react';
-import { ChakraProvider, Box, VStack, Grid, theme } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  Box,
+  VStack,
+  Grid,
+  extendTheme,
+} from '@chakra-ui/react';
 import PodCastSearch from './pages/PodCastSearch';
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  },
+  fonts: {
+    body: 'PPNeueMontreal, sans-serif',
+  },
+  colors: {
+    Main: {
+      600: '#ff5900',
+    },
+
+    Secondary: {
+      Sunny600: '#F78104',
+      Yellow600: '#FAAB36',
+      Teal600: '#005F60',
+      Grey200: '#F5F3EE',
+      Black: '#000000',
+      White: '#FFFFFF',
+    },
+
+    TigerOrange: {
+      200: '#FFCCA3',
+      400: 'FFA97A',
+    },
+    grey: {
+      600: '#A3A099',
+      700: '#797670',
+    },
+    KindaYellow: {
+      300: '#FCDEB0',
+      500: '#FABD5E',
+      700: '#D4902B',
+    },
+  },
+});
 
 function App() {
   return (
