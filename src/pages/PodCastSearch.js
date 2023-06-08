62bd8106798f37a6bb73e4e82957278bef3dab77
dev9sparsh <sparsh.dev9@gmail.com>
2023-06-09 05:15:37 +0530

    state management for card data , prop communication with search tool bar and card component , mixing mock data with Api driven data

--- src/pages/PodCastSearch.js
+++ src/pages/PodCastSearch.js
@@ -1,6 +1,6 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { SimpleGrid } from '@chakra-ui/react';
+
 import { useLazyQuery } from '@apollo/client';
 
 import SearchToolBar from '../components/SearchToolBar';
@@ -8,26 +8,49 @@ import PodcastCard from '../components/PodcastCard';
 import { GET_PODCASTS } from '../api/query';
 
 const PodCastSearch = () => {
+  const [podcasts, setPodcasts] = useState([]);
+
   const [getPodcasts] = useLazyQuery(GET_PODCASTS, {
     onCompleted: data => {
-      console.log(data);
+      setPodcasts(data?.contentCards?.edges);
     },
     fetchPolicy: 'no-cache',
   });
 
-  useEffect(() => {
-    getPodcasts({
-      variables: { limit: 5, searchKeyword: '' },
-    });
-  }, []);
-
   return (
     <>
-      <SearchToolBar />
+      <SearchToolBar ApiHook={getPodcasts} />
       <SimpleGrid columns={[1, 2, 2, 3]} spacingX="50px" spacingY="50px" p={5}>
-        {Array.from({ length: 6 }, (_, index) => index).map(item => {
-          return <PodcastCard />;
-        })}
+        {podcasts &&
+          Array.isArray(podcasts) &&
+          podcasts.length !== 0 &&
+          podcasts.map(item => {
+            const mockifiedItem = {
+              ...item,
+              // here hardcoding some texts for display Purpose.
+              text: `${
+                item.experts?.length > 0
+                  ? `Peak Performance: Going From Good to Great with ${item?.experts[0].firstName} ${item?.experts[0].lastName}`
+                  : `Peak Performance: Going From Good to Great.`
+              }`,
+              text2: `${
+                item.experts?.length > 0
+                  ? `${item?.experts[0]?.company || 'Subway'} ${
+                      item?.experts[0]?.title || 'APAC'
+                    }`
+                  : `Peak Performance Going From Good to Great.`
+              }`,
+              text23: `${
+                item.experts?.length > 0
+                  ? `${item?.experts[0].firstName} ${item?.experts[0].lastName}`
+                  : `Peak Performance Going From Good to Great.`
+              }`,
+              title: `communicating as a leader`,
+            };
+            return (
+              <PodcastCard edgeTileInfo={mockifiedItem} key={item?.name} />
+            );
+          })}
       </SimpleGrid>
     </>
   );
