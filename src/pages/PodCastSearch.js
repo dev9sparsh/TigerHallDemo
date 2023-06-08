0f9307a999db241ab70fe38fa137a7e8f634af0c
dev9sparsh <sparsh.dev9@gmail.com>
2023-06-09 03:42:44 +0530

    hooking query call on mounting useEfect of PodCastSearch component

--- src/pages/PodCastSearch.js
+++ src/pages/PodCastSearch.js
@@ -1,10 +1,26 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
 import { SimpleGrid } from '@chakra-ui/react';
+import { useLazyQuery } from '@apollo/client';
 
 import SearchToolBar from '../components/SearchToolBar';
 import PodcastCard from '../components/PodcastCard';
+import { GET_PODCASTS } from '../api/query';
 
 const PodCastSearch = () => {
+  const [getPodcasts] = useLazyQuery(GET_PODCASTS, {
+    onCompleted: data => {
+      console.log(data);
+    },
+    fetchPolicy: 'no-cache',
+  });
+
+  useEffect(() => {
+    getPodcasts({
+      variables: { limit: 5, searchKeyword: '' },
+    });
+  }, []);
+
   return (
     <>
       <SearchToolBar />
